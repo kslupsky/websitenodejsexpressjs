302c4bf972efbae893c105b79732918ea91eb4fe
kslupsky <kealy.slupsky@pqa.ca>
2018-10-12 17:01:03 -0300

    moving machines

--- conference/server/routes/speakers/index.js
+++ conference/server/routes/speakers/index.js
@@ -6,7 +6,7 @@ module.exports = () => {
         return res.render('speakers');
     });
     router.get('/:name', (req, res, next) => {
-        return res.send(`Speaker detail page for ${req.params.name}`);
+        return res.render('speakers/detail');
     });
     return router;
 };
