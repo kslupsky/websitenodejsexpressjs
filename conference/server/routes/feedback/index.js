3eafef792430fa41ac26fcab87c24df47ee5809c
Kealy Slupsky <kslupsky@pqatesting.com>
2018-10-15 14:22:55 -0300

    changes after 06_03

--- conference/server/routes/feedback/index.js
+++ conference/server/routes/feedback/index.js
@@ -9,16 +9,36 @@ module.exports = (param) => {
             const feedbacklist = await feedbackService.getList();
             return res.render('feedback', {
                 page: 'Feedback',
-                feedbacklist
+                feedbacklist,
+                success: req.query.success
             });
         } catch(err) {
             return next(err);
         }
     });
 
-    router.post('/', (req, res, next) => {
-        console.log(req.body);        
-        return res.send('Form sent');
+    router.post('/', async (req, res, next) => {
+        try {
+            const fbName = req.body.fbName.trim();
+            const fbTitle = req.body.fbTitle.trim();
+            const fbMessage = req.body.fbMessage.trim();
+            const feedbacklist = await feedbackService.getList();
+    
+            if(!fbName || !fbTitle || ! fbMessage) {
+                return res.render('feedback', {
+                    page: 'Feedback',
+                    error: true,
+                    fbName,
+                    fbMessage,
+                    fbTitle,
+                    feedbacklist
+                });
+            }
+
+            return res.redirect('/feedback?success=true');
+        } catch(err) {
+            return next(err);
+        }
     });
 
     return router;
